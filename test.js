1a428c675f18181f576e8984676d420c0738556e
Andreas Gillström <andreasgillstrom@gmail.com>
2015-10-05 07:15:03 +0200

    Tweak tests

--- test.js
+++ test.js
@@ -1,33 +1,46 @@
-'use strict';
-var test = require('ava');
-var appSize = require('./');
+import test from 'ava';
+import appSize from './';
 
 test('path', t => {
 	t.plan(1);
 
-	appSize('/Applications/Safari.app').then(size => {
-		t.assert(typeof size === 'number');
-	}).catch(err => {
-		t.assert(!err, err);
-	});
+	appSize('/Applications/Safari.app')
+		.then(size => {
+			t.is(typeof size, 'number');
+		})
+		.catch(err => {
+			t.assert(!err, err);
+		});
 });
 
 test('name', t => {
 	t.plan(1);
 
-	appSize('Safari').then(size => {
-		t.assert(typeof size === 'number');
-	}).catch(err => {
-		t.assert(!err, err);
-	});
+	appSize('Safari')
+		.then(size => {
+			t.is(typeof size, 'number');
+		})
+		.catch(err => {
+			t.assert(!err, err);
+		});
 });
 
 test('bundle', t => {
 	t.plan(1);
 
-	appSize('com.apple.Safari').then(size => {
-		t.assert(typeof size === 'number');
-	}).catch(err => {
-		t.assert(!err, err);
+	appSize('com.apple.Safari')
+		.then(size => {
+			t.is(typeof size, 'number');
+		})
+		.catch(err => {
+			t.assert(!err, err);
+		});
+});
+
+test('bundle', t => {
+	t.plan(1);
+
+	appSize('com.apple.bundle.that.doesnt.exist').catch(err => {
+		t.assert(err, err);
 	});
 });
