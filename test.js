01f6ee8dc766b91e037b42bd31181680a1b43658
Andreas Gillström <andreasgillstrom@gmail.com>
2016-01-11 21:28:14 +0100

    Improve code style

--- test.js
+++ test.js
@@ -5,42 +5,28 @@ test('path', t => {
 	t.plan(1);
 
 	appSize('/Applications/Safari.app')
-		.then(size => {
-			t.is(typeof size, 'number');
-		})
-		.catch(err => {
-			t.assert(!err, err);
-		});
+		.then(size => t.is(typeof size, 'number'))
+		.catch(err => t.assert(!err, err));
 });
 
 test('name', t => {
 	t.plan(1);
 
 	appSize('Safari')
-		.then(size => {
-			t.is(typeof size, 'number');
-		})
-		.catch(err => {
-			t.assert(!err, err);
-		});
+		.then(size => t.is(typeof size, 'number'))
+		.catch(err => t.assert(!err, err));
 });
 
 test('bundle', t => {
 	t.plan(1);
 
 	appSize('com.apple.Safari')
-		.then(size => {
-			t.is(typeof size, 'number');
-		})
-		.catch(err => {
-			t.assert(!err, err);
-		});
+		.then(size => t.is(typeof size, 'number'))
+		.catch(err => t.assert(!err, err));
 });
 
 test('bundle', t => {
 	t.plan(1);
 
-	appSize('com.apple.bundle.that.doesnt.exist').catch(err => {
-		t.assert(err, err);
-	});
+	appSize('com.apple.bundle.that.doesnt.exist').catch(err => t.assert(err, err));
 });
