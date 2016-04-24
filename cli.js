6d396598bdb23ac70634d54157ea012f3c91385d
Andreas Gillström <andreasgillstrom@gmail.com>
2016-04-24 12:09:36 +0200

    Bump AVA version

--- cli.js
+++ cli.js
@@ -1,28 +1,19 @@
 #!/usr/bin/env node
 'use strict';
-var meow = require('meow');
-var appSize = require('./');
-var prettyBytes = require('pretty-bytes');
+const meow = require('meow');
+const appSize = require('./');
+const prettyBytes = require('pretty-bytes');
 
-var cli = meow({
-	help: [
-		'Examples',
-		'	$ app-size Safari',
-		'	$ app-size /Applications/Safari.app',
-		'	$ app-size com.apple.Safari'
-	]
-});
+const cli = meow(`
+	Examples
+		$ app-size Safari
+		$ app-size /Applications/Safari.app
+		$ app-size com.apple.Safari
+`);
 
 if (!cli.input.length) {
 	console.error('Application is required');
 	process.exit(1);
 }
 
-appSize(cli.input[0])
-	.then(function (res) {
-		console.log(prettyBytes(res));
-	})
-	.catch(function (err) {
-		console.error(err.message);
-		process.exit(1);
-	});
+appSize(cli.input[0]).then(res => console.log(prettyBytes(res)))
--- test.js
+++ test.js
@@ -1,32 +1,14 @@
 import test from 'ava';
-import appSize from './';
+import m from './';
 
-test('path', t => {
-	t.plan(1);
-
-	appSize('/Applications/Safari.app')
-		.then(size => t.is(typeof size, 'number'))
-		.catch(err => t.assert(!err, err));
+test('path', async t => {
+	t.is(typeof (await m('/Applications/Safari.app')), 'number');
 });
 
-test('name', t => {
-	t.plan(1);
-
-	appSize('Safari')
-		.then(size => t.is(typeof size, 'number'))
-		.catch(err => t.assert(!err, err));
+test('name', async t => {
+	t.is(typeof (await m('Safari')), 'number');
 });
 
-test('bundle', t => {
-	t.plan(1);
-
-	appSize('com.apple.Safari')
-		.then(size => t.is(typeof size, 'number'))
-		.catch(err => t.assert(!err, err));
-});
-
-test('bundle', t => {
-	t.plan(1);
-
-	appSize('com.apple.bundle.that.doesnt.exist').catch(err => t.assert(err, err));
+test('bundle', async t => {
+	t.is(typeof (await m('com.apple.Safari')), 'number');
 });
